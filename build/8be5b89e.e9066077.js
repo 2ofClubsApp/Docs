82fb7f69157aafdeeb66e600cc785979685efde7
Chris Lim <hello@hiimchrislim.co>
2020-07-23 23:28:13 -0400

    Remove build

--- build/8be5b89e.e9066077.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{119:function(a){a.exports=JSON.parse('{"backend":{"allTagsPath":"/blog/tags","slug":"backend","name":"backend","count":4,"permalink":"/blog/tags/backend"},"roadmap":{"allTagsPath":"/blog/tags","slug":"roadmap","name":"roadmap","count":1,"permalink":"/blog/tags/roadmap"}}')}}]);
